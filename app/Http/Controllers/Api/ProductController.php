5742c1f3aa43793cebaac76fb266809a5c5f5653
LaysR3B <laysrbz@gmail.com>
2025-10-15 22:45:33 -0500

    Paso 1-4

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
 use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
@@ -49,7 +54,7 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        $product = Product::findOrFail($id)
+        $product = Product::findOrFail($id);
         $product->delete();
         return response()->json(null, 204);
     }
